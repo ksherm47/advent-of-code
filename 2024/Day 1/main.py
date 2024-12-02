f2eb8147a6c93e7f585adea52477ea67ba391b33
Kenneth Sherman <kennethsherm@gmail.com>
2024-12-02 13:51:49 -0600

    Day 1 submission

---
+++ 2024/Day 1/main.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+INPUT_FILE = 'input.txt'
+
+
+def main():
+    with open(INPUT_FILE) as fr:
+        left, right = zip(*[map(int, line.split()) for line in fr])
+
+    # Part 1
+    total_dist = sum([abs(l - r) for l, r in zip(sorted(left), sorted(right))])
+    print(total_dist)
+
+    # Part 2
+    counts = Counter(right)
+    similarity_score = sum([l * counts[l] for l in left])
+    print(similarity_score)
+
+
+if __name__ == '__main__':
+    main()
